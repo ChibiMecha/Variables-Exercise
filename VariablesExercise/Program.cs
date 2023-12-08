1e43c9d4b1090b83a5459b26dbd7cd5744cae834
Vernon Hudson <vhudson@crimson.ua.edu>
2023-12-08 14:53:56 -0600

    Declared and initialized variables types. Interpolated variables to fix errors and run program. Success

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int model = 6;
+            char bundle = 'A';
+            bool isBundled = true;
+            double discount = 20.00;
+            decimal msrp = 799.99m;
+            string name;
+            name = "Playstation";
+
+            Console.WriteLine($"Lemme get ah {name} {model}\n");
+            Console.WriteLine($"Cashier: Which one?\n");
+            Console.WriteLine($"Does bundle {bundle} get the {discount} percent off?\n");
+            Console.WriteLine($"Cashier: Yes! Its {isBundled} so that we can offer more extras for lower prices.\n");
+            Console.WriteLine($"Great! I'll take the one for msrp {msrp}. Bag it up my boy"); //will use \n when appropiate from now on 
         }
     }
 }
